81646edeea56a843b0aa2925561d039569526836
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2024-07-29 10:11:06 +0300

    Generate NPM package

---
+++ react-native-mesh-gradient/src/index.tsx
@@ -0,0 +1,36 @@
+import {
+  requireNativeComponent,
+  UIManager,
+  Platform,
+  type ViewProps,
+} from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-mesh-gradient' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+  type MeshGradientProps = ViewProps & {
+    meshWidth: number;
+    meshHeight: number;
+    points: number[][];
+    primaryColors: string[];
+    secondaryColors: string[];
+    background?: string;
+    smoothsColors?: boolean;
+    colorSpace?: "device" | "perceptual";
+    isAnimated?: boolean;
+    borderRadius?: number;
+    animationDuration?: number;
+    animationType?: "sine" | "easeInOut";
+  };
+  
+const ComponentName = 'MeshGradientView';
+
+export const MeshGradientView =
+  UIManager.getViewManagerConfig(ComponentName) != null
+    ? requireNativeComponent<MeshGradientProps>(ComponentName)
+    : () => {
+        throw new Error(LINKING_ERROR);
+      };
